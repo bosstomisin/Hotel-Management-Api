c5b4cd3c7a5465235f28e2cfa4d9a2867490c1ed
Tomisin <tadereti@gmail.com>
2022-01-31 22:26:02 +0100

    implemented EfCore

---
+++ Hotel-management-Api.Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Hotel_management_Api.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext()
+        {
+
+        }
+    }
+}
--- Hotel-management-Api/Models/User.cs
+++ Hotel-management-Api/Models/User.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Hotel_management_Api.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public string UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public List<Room> Rooms { get; set; } = new List<Room>();
     }
 }
