c2f09082726be16f116959d8cd0936b90e58f43e
Tomisin <tadereti@gmail.com>
2022-01-31 18:52:21 +0100

    added models

---
+++ Hotel-management-Api/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_management_Api.Models
+{
+    public class Room
+    {
+        public string RoomId { get; set; }
+        public string RoomType { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
---
+++ Hotel-management-Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_management_Api.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
