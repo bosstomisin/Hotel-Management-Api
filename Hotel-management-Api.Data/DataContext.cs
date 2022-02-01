ccfe6946b2fdd2b3bb4bed4a632f5d9baf99ce97
Tomisin <tadereti@gmail.com>
2022-02-01 12:25:36 +0100

    moved model folder

--- Hotel-management-Api.Data/DataContext.cs
+++ Hotel-management-Api.Data/DataContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Hotel_management_Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Hotel_management_Api.Data
 {
     public class DataContext : IdentityDbContext<User>
     {
-        public DataContext()
+        public DataContext(DbContextOptions<DataContext> options ) : base(options)
         {
 
         }
+        public DbSet<Room> Room { get; set; }
     }
 }
--- Hotel-management-Api/Startup.cs
+++ Hotel-management-Api/Startup.cs
@@ -1,7 +1,9 @@
+using Hotel_management_Api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,12 +28,13 @@ namespace Hotel_management_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel_management_Api", Version = "v1" });
             });
+            services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
