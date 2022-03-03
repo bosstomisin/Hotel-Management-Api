7e30bf85402e67bd525ac4ad6a07ab44764f95f6
Tomisin <tadereti@gmail.com>
2022-03-03 22:32:59 +0100

    added jwtservice

---
+++ Hotel-management-Api.Data/Models/JWTData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_management_Api.Data.Models
+{
+    public class JWTData
+    {
+        public TimeSpan TokenLifeTime { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+    }
+}
---
+++ Hotel-management-Api/Service/Implementation/JWTService.cs
@@ -0,0 +1,49 @@
+﻿using Hotel_management_Api.Data.Models;
+using Hotel_management_Api.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_management_Api.Service.Implementation
+{
+    public class JWTService
+    {
+       
+        public static string GenerateToken(AppUser user, string role, IOptions<JWTData> option)
+        {
+            var jwtData = option.Value;
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //foreach (var role in userRoles)
+            //{
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            //}
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtData.SecretKey));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now + jwtData.TokenLifeTime,
+                Audience = jwtData.Issuer,
+                Issuer = jwtData.Issuer,
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
