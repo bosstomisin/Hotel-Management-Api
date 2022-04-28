99d0259d20278ebbaf0a4d6a667a8622c911bf2f
Tomisin <tadereti@gmail.com>
2022-04-28 19:46:00 +0100

    integrated paystack

---
+++ Hotel-management-Api/Service/Implementation/PaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_management_Api.Service.Implementation
+{
+    public class PaymentService
+    {
+       // public async Task<BaseResponse<PaymentResponse>> MakePayment()
+    }
+}
